749b5326f8d2fb4f2724ad98d199f2123cbb3253
Michael Dorian <doby162@gmail.com>
2020-12-15 18:06:44 -0600

    add file based persistance to high score

--- server/main.go
+++ server/main.go
@@ -3,15 +3,19 @@ package main
 import (
 	"fmt"
 	"github.com/gorilla/mux"
+	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 	"strconv"
 )
 
 var total int
 
 func main() {
-	total = 1000
+	tot := ReadFromFile("highscore")
+	total, _ = strconv.Atoi(tot)
 	r := mux.NewRouter()
 	r.HandleFunc("/", landing_page)
 	r.HandleFunc("/{category}", landing_page)
@@ -26,8 +30,30 @@ func landing_page(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	newval, _ := strconv.Atoi(vars["category"])
 	if vars["category"] != "" && newval < total {
+		_ = WriteToFile("highscore", strconv.Itoa(newval))
 		tot := &total
 		*tot = newval
 	}
 	fmt.Fprintf(w, "%v", total)
+}
+
+func WriteToFile(filename string, data string) error {
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	_, err = io.WriteString(file, data)
+	if err != nil {
+		return err
+	}
+	return file.Sync()
+}
+func ReadFromFile(filename string) string {
+	ret, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return "1000" // if the file doens't exist return the default
+	}
+	return string(ret)
 }
